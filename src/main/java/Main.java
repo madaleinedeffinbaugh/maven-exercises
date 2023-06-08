344d81055a34bc7dfc32ab6d10ad373bfa4b060d
Madaleine Deffinbaugh <madaleine.deffinbaugh@gmail.com>
2023-06-08 11:06:38 -0600

    forgot to commit and push - exercise complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,22 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter something: ");
+        String input = scanner.nextLine();
+
+        System.out.println("You Entered: \"" + input + "\"");
+
+        if (StringUtils.isNumeric(input)) {
+            System.out.println("\"" + input + "\" is a number");
+        } else {
+            System.out.println("\"" + input + "\" is not a number");
+        }
+        System.out.println("Flipped Case: " + StringUtils.swapCase(input));
+        System.out.println("Reversed: " + StringUtils.reverse(input));
     }
 }
